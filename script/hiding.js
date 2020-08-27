a635b6fcbe6804547258d30ab7fe67edb033567c
kyeongha <hkh82002857@gmail.com>
2020-08-27 18:33:51 +0900

    hiding

--- script/hiding.js
+++ script/hiding.js
@@ -2,4 +2,12 @@ const btn = document.querySelector("#my-btn");
 
 const HIDING_CL ="hiding";
 
-btn.classList.toggle(HIDING_CL);
+
+function hidingFN(){
+    btn.classList.toggle(HIDING_CL);
+}
+
+function init(){
+    btn.addEventListener("click",hidingFN)
+}
+init();
